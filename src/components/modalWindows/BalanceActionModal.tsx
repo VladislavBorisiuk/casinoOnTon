ea0502b596cca0bc676e8d054bd92a748396fb97
влад борисюк <gaimer433@gmail.com>
2025-04-21 16:19:55 +0300

    balanceUpdateVisual

--- src/components/modalWindows/BalanceActionModal.tsx
+++ src/components/modalWindows/BalanceActionModal.tsx
@@ -31,28 +31,35 @@ const BalanceActionModal: React.FC<BalanceActionModalProps> = ({
       <div className={isOpen ? 'modal-content active' : 'modal-content'}>
         <h2 className="text-lg font-semibold mb-4">{title}</h2>
         <input
-          type="number"
-          min="0.2"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          placeholder={placeholder}
-          className="w-full mb-4 p-2 border rounded"
-        />
-        <div className="flex justify-end gap-4"> {/* Увеличен отступ между кнопками */}
-          <button
-            onClick={onClose}
-            className="px-3 py-1 bg-gray-300 rounded"
-          >
-            Отмена
-          </button>
-          <button
-            onClick={onConfirm}
-            className="px-3 py-1 bg-green-500 text-white rounded"
-            disabled={loading}
-          >
-            {loading ? 'Обработка...' : confirmLabel}
-          </button>
-        </div>
+  type="number"
+  min="0.2"
+  value={amount}
+  onChange={(e) => {
+    const newValue = parseFloat(e.target.value);
+    if (newValue >= 0.2 || e.target.value === "") { // Если новое значение >= 0.2 или поле пустое
+      setAmount(e.target.value);
+    }
+  }}
+  placeholder={placeholder}
+  className="w-full mb-4 p-2 border rounded"
+/>
+
+<div className="flex justify-end button-container"> {/* Используем новый класс button-container */} 
+  <button
+    onClick={onClose}
+    className="px-3 py-1 bg-gray-300 rounded"
+  >
+    Отмена
+  </button>
+  <button
+    onClick={onConfirm}
+    className="px-3 py-1 bg-green-500 text-white rounded"
+    disabled={loading}
+  >
+    {loading ? 'Обработка...' : confirmLabel}
+  </button>
+</div>
+
       </div>
     </div>
   );
